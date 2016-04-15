06b26211764c13295459faae44bb089fa0ff78dd
ChinmayAmin <chinmayhamin@gmail.com>
2016-04-14 23:45:11 -0400

    Front page GUI done properly using flexbox

--- AwesomeProject/Reddit.js
+++ AwesomeProject/Reddit.js
@@ -6,7 +6,8 @@ import React, {
   View,
   TouchableHighlight,
   Image,
-  ListView
+  ListView,
+  Picker
 } from 'react-native';
 
 var _ = require('lodash');
@@ -17,29 +18,13 @@ export default class AwesomeProject extends Component {
     var ds = new ListView.DataSource({
       rowHasChanged: (r1, r2) => r1 != r2
     });
-    var subreddit = 'all';
 
-    this.getSubredditsData(subreddit).then(function(data) {
-      console.log(data);
-    })
-    // console.log(this.getSubredditsData('all'));
     this.state = {
       ds: this.getSubredditData(),
       dataSource:ds,
-      subreddit: subreddit
-    }
+	  subreddit: ''
   }
-
-  async getSubredditsData(subredditName) {
-    try {
-      let response = await fetch('http://reddit.com/r/' + subredditName + '/.json');
-      let responseJson = await response.json();
-      return responseJson;
-    } catch(error) {
-      // Handle error
-      console.error(error);
-    }
-  }
+}
 
   getSubredditData() {
     var data = [];
@@ -49,7 +34,7 @@ export default class AwesomeProject extends Component {
     return data;
   }
 
-  componentDidMount(){
+  componentDidMount() {
     this.setState({
       dataSource:this.state.dataSource.cloneWithRows(this.state.ds)
     })
@@ -70,19 +55,51 @@ export default class AwesomeProject extends Component {
 
   renderHeaderSearch() {
     return (
-      <TouchableHighlight onPress={this.headerSearchButtonsPressed} underlayColor ='rgba(0,0,0,0)'>
-          <Image
-            source={{uri: './hamburger.png'}}
-            style={styles.thumbnail}
-          />
-      </TouchableHighlight>
+		<View style={styles.headerSearch}>
+			<View style={styles.hamburger}>
+      			<TouchableHighlight onPress={this.headerSearchButtonsPressed}>
+					<Image source={require('./hamburger.png')} style={{height: 30, width: 30}}/>			 
+    	  		</TouchableHighlight>
+			</View>
+			<View style={styles.subredditDropdown}>	
+				<Picker
+					 selectedValue={this.state.subreddit}
+  					 onValueChange={(subreddit) => this.setState({subreddit: subreddit})}>
+					 <Picker.Item label="Front Page" value="Front Page" />
+  					 <Picker.Item label="All" value="js" />
+				</Picker>
+			</View>
+			<View style={styles.space}/>
+			<View style={styles.search}>
+				<TouchableHighlight onPress={this.headerSearchButtonsPressed}>
+					<Image source={require('./search.png')} style={{height: 35, width: 35}}/>			 
+				</TouchableHighlight>
+			</View>
+			<View style={styles.refresh}>
+				<TouchableHighlight onPress={this.headerSearchButtonsPressed}>
+					<Image source={require('./refresh.png')} style={{height: 30, width: 30}}/>			 
+    	  		</TouchableHighlight>
+			</View>
+			<View style={styles.sidebar}>
+				<TouchableHighlight onPress={this.headerSearchButtonsPressed}>
+					<Image source={require('./sidebar.png')} style={{height: 30, width: 30}}/>			 
+    	  		</TouchableHighlight>
+			</View>
+		</View>
     );
   }
 
   renderHeaderFilter() {
     return (
-      <View>
-      </View>
+     <View style={styles.headerFilter}>
+      	<TouchableHighlight onPress={this.headerSearchButtonsPressed}>
+      	    <Text style={styles.headerFilterText}> What's hot </Text>
+    	</TouchableHighlight>	
+		<View style={styles.filterSpace}/>
+      	<TouchableHighlight onPress={this.headerSearchButtonsPressed}>
+			<Image source={require('./filter.png')} style={{height: 35, width: 35}}/>			 
+    	</TouchableHighlight>
+	</View>
     );
   }
 
@@ -101,7 +118,7 @@ export default class AwesomeProject extends Component {
 
   renderRow(rowData){
     return (
-      <TouchableHighlight onPress={()=> this.pressRow(rowData)} underlayColor = '#ddd'>
+      <TouchableHighlight onPress={()=> this.pressRow(rowData)} underlayColor = '#3D3D3D'>
         <View style ={styles.row}>
           <Text style={{fontSize:18}}> {rowData} </Text>
         </View>
@@ -124,19 +141,75 @@ export default class AwesomeProject extends Component {
 const styles = StyleSheet.create({
     container: {
       flex: 1,
-      flexDirection: 'column'
+      flexDirection: 'column',
+	  alignSelf: "stretch"
     },
     header: {
-      flex: .15
+      flex: .15,
+	  alignSelf: "stretch"
     },
+	headerSearch: {
+		flex: 1,
+		flexDirection: 'row',
+		alignSelf: "stretch",
+		backgroundColor: '#5C5C5C'
+	},
+	hamburger: {
+		paddingTop: 10,
+		paddingLeft: 15,
+		paddingRight: 15	
+	},
+	subredditDropdown: {
+		width: 145
+	},
+	space: {
+		width: 50
+	},
+	search: {
+		paddingTop: 7,
+		paddingRight: 5,
+		width: 35,
+		height: 35
+	},
+	refresh: {
+		paddingTop: 8,
+		width: 30,
+		height: 30
+	},
+	sidebar: {
+		paddingTop: 8,
+		width: 35,
+		height: 35
+	},
+	headerFilter: {
+		flex: 1,
+		flexDirection: 'row',
+		alignSelf: "stretch",
+		backgroundColor: '#7A7A7A',
+		paddingTop: 15,
+		paddingLeft: 15
+	},
+	headerFilterText: {
+		width: 150,
+		fontSize: 18
+	},
+	filterSpace: {
+		width: 145
+	},
+	headerFilterDropdown: {
+		width: 35,
+		height: 35
+	},
     content: {
-      flex: .75
+      flex: .75,
+	  alignSelf: "stretch",
+	  backgroundColor: '#9087BD'
     },
     row:{
       flex:1,
       flexDirection:'row',
       padding:18,
-      borderBottomWidth: 1,
+      borderBottomWidth: 0.2,
       borderColor: '#d7d7d7',
     }
 });
